566c162949f9ad24e1ef6653463781d5d287b3e4
西村唯 <ynishimura@nishimurayuinoMacBook-Air.local>
2019-07-26 14:03:44 +0900

    モデルのテストを追加

--- tests/test_guestboard_model.py
+++ tests/test_guestboard_model.py
@@ -4,7 +4,7 @@ from django.db import utils
 
 class PostingModelTest(TestCase):
  def test_01(self):
-     """"Create Normal Pattern"""
+     """"Create Success Pattern"""
      name = "aaa"
      message = "message01"
      Posting.objects.create(name=name, message=message)
@@ -17,16 +17,8 @@ class PostingModelTest(TestCase):
          Posting.objects.create(name=name, message=message)
 
  def test_03(self):
-     """Create Error Pattern3: mesage is not null"""
-     name = "AAA"
-     message = None
-     with self.assertRaises(utils.IntegrityError):
-         Posting.objects.create(name=name, message=message)
-
- def test_04(self):
-     """Create Error Pattern3; name length under 65"""
-     name = "a" * 65
-     print("name = {}, length={}".format(name, len(name)))
-     message = "message 04"
-    #  with self.assertRaises(ValueError):
-     Posting.objects.create(name=name, message=message)
+    """Create Error Pattern3: mesage is not null"""
+    name = "AAA"
+    message = None
+    with self.assertRaises(utils.IntegrityError):
+        Posting.objects.create(name=name, message=message)
