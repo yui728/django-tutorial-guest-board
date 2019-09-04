731fd91cf4cfe1aed965efe83fd130f6f7ac809e
yui728 <yui.nishimra.1984@gmail.com>
2019-09-04 17:14:17 +0900

    静的ファイルに関する設定を追加

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('guestboard/', include('guestboard.urls'))
+    path('guestboard/', include('guestboard.urls')),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
